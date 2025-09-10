18a1a360221202ca04a47787943c17f678aecec6
Hans Georg Schaathun <georg@schaathun.net>
2025-09-10 21:41:29 +0200

    Script to build moodle directory

---
+++ gitclone.sh
@@ -0,0 +1,5 @@
+
+git clone -b MOODLE_500_STABLE https://github.com/moodle/moodle.git
+( cd moodle/question/type && git clone https://github.com/trampgeek/moodle-qtype_coderunner.git coderunner )
+
+( cd moodle/question/behaviour/ && git clone https://github.com/trampgeek/moodle-qbehaviour_adaptive_adapted_for_coderunner.git adaptive_adapted_for_coderunner )
